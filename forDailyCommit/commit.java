46d51a5c3d5a52dd49e203eead3bb30a6691db40
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-09-03 21:55:18 +0800

    Added a practice project (BMI Test) in commit.java

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -1,20 +1,39 @@
 package forDailyCommit;
+import java.util.Scanner;
 
 public class commit {
     public static void main(String[] args) {
-        
-        String[][] crushes = {
-            {"Sandra", "Smiley", "Zahra"}, 
-            {"Bianca", "Dessirre", "Minseon"},
-            {"Ginalyn", "Michaela", "Lourelie"}
-        };
+        Scanner scan = new Scanner(System.in);
 
-        for (int i = 0; i < crushes.length; i++) {
-            for (int j = 0; j < crushes[i].length; j++) {
-                System.out.print(crushes[i][j]+" ");
-            }
-            System.out.println();
+        System.out.printf("Enter height (in cm): ");
+        float enterheight = scan.nextFloat();
+
+        System.out.printf("Enter weight (in kg): ");
+        float enterWeight = scan.nextFloat();
+
+        float finalHeight = getFinalHeight(enterheight);
+
+        float bmiResult = enterWeight / finalHeight;
+
+        System.out.printf("\nResult: %.1f\n", bmiResult);
+
+        if (bmiResult < 18.0) {
+            System.out.println("UNDERWEIGHT");
+        }
+        else if (bmiResult < 25.0) {
+            System.out.println("NORMAL WEIGHT");
+        }
+        else if (bmiResult < 29.9) {
+            System.out.println("OVERWEIGHT");
+        }
+        else {
+            System.out.println("OBESE");
         }
     }
-}
 
+    public static float getFinalHeight(float enterHeight) {
+        enterHeight = enterHeight / 100;
+        float result = enterHeight * enterHeight;
+        return result;
+    }
+}
