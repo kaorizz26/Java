3d1dff852df31446d3560ac0dd6abb8cc64117e5
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-09-05 20:02:09 +0800

    Modified practice.java (did some practice projects)

--- practice/practice.java
+++ practice/practice.java
@@ -1,64 +1,24 @@
 package practice;
-
+import java.util.Random;
 import java.util.Scanner;
 
 public class practice {
     public static void main(String[] args) {
+        Random random = new Random();
         Scanner scan = new Scanner(System.in);
-        String[] things = new String[10]; // Initialize with a fixed size
 
-        while (true) {
-            System.out.println("\n\tDigital cart:");
-            System.out.println("1. Add something.");
-            System.out.println("2. Show cart list.");
-            System.out.println("3. Remove something.");
-            System.out.println("4. Quit");
+        System.out.printf("Enter minimum number: ");
+        int min = scan.nextInt();
 
-            System.out.print("Enter choice (number): ");
-            int choice = scan.nextInt();
-            scan.nextLine();
+        System.out.printf("Enter maximum number: ");
+        int max = scan.nextInt();
 
-            switch (choice) {
-                case 1:
-                    System.out.print("Add something: ");
-                    String addThings = scan.nextLine();
-
-                    for (int i = 0; i < things.length; i++) {
-                        if (things[i] == null) {
-                            things[i] = addThings;
-                            break;
-                        }
-                    }
-                    break;
-
-                case 2:
-                    System.out.println("\nCart list:");
-                    for (String thing : things) {
-                        if (thing != null) {
-                            System.out.println(thing);
-                        }
-                    }
-                    break;
-
-                case 3:
-                    System.out.print("Remove something: ");
-                    String removeThings = scan.next();
-
-                    for (int i = 0; i < things.length; i++) {
-                        if (things[i] != null && things[i].equals(removeThings)) {
-                            things[i] = null;
-                            break;
-                        }
-                    }
-                    break;
-
-                case 4:
-                    return;
-
-                default:
-                    System.out.println("Invalid input.");
-                    break;
-            }
+        if (max < min) {
+            System.out.println("Max number should be less than min.");
         }
+
+        int result = random.nextInt(max - min + 1) + min;
+
+        System.out.println(result);
     }
 }
