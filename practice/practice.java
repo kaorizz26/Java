a40f0bc4811b0182e7990a7d8c6914635ae44afc
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-09-07 22:23:04 +0800

    modified practice.java, practice some codes

--- practice/practice.java
+++ practice/practice.java
@@ -4,21 +4,34 @@ import java.util.Scanner;
 
 public class practice {
     public static void main(String[] args) {
-        Random random = new Random();
         Scanner scan = new Scanner(System.in);
 
-        System.out.printf("Enter minimum number: ");
-        int min = scan.nextInt();
+        System.out.printf("Enter number 1: ");
+        int n1 = scan.nextInt();
 
-        System.out.printf("Enter maximum number: ");
-        int max = scan.nextInt();
+        System.out.printf("Enter number 2: ");
+        int n2 = scan.nextInt();
 
-        if (max < min) {
-            System.out.println("Max number should be less than min.");
+        System.out.printf("Enter number 3: ");
+        int n3 = scan.nextInt();
+
+        if (n1 <= n2 && n2 <= n3) {
+            System.out.printf("%d %d %d\n", n1, n2, n3);
         }
-
-        int result = random.nextInt(max - min + 1) + min;
-
-        System.out.println(result);
+        else if (n1 <= n3 && n3 <= n2) {
+            System.out.printf("%d %d %d\n", n1, n3, n2);
+        }
+        else if (n2 <= n1 && n1 <= n3) {
+            System.out.printf("%d %d %d\n", n2, n1, n3);
+        }        
+        else if (n2 <= n3 && n3 <= n1) {
+            System.out.printf("%d %d %d\n", n2, n3, n1);
+        }
+        else if (n3 <= n1 && n1 <= n2) {
+            System.out.printf("%d %d %d\n", n3, n1, n2);
+        }
+        else {
+            System.out.printf("%d %d %d\n", n3, n2, n1);
+        }
     }
 }
