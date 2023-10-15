ca3aed3d859704454972d2aef1201dc0c876f3ad
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-10-15 23:19:01 +0800

    Code practice (input three items and quantity, output total cost of each three and overall cost of all)

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -4,36 +4,36 @@ import java.util.Scanner;
 public class commit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        double overallTotalCost = 0.0;
+        int numItems = 3;
+        double[] prices = new double[numItems];
+        double[] quantities = new double[numItems];
+        double[] totalCost = new double[numItems];
+        
+        for (int i = 0; i < numItems; i++) {
+            System.out.print("\nEnter price of item " + (i + 1) + ": ");
+            prices[i] = scan.nextDouble();
+            System.out.print("Quantity: ");
+            quantities[i] = scan.nextDouble();
 
-        System.out.printf("Enter height (in cm): ");
-        float enterheight = scan.nextFloat();
+            if (prices[i] < 0 || quantities[i] < 0) {
+                System.out.println("Price and quantity must be non-negative. Please re-enter.");
+                i--; // Decrement i to repeat the current item input.
+            } else {
+                double Cost = prices[i] * quantities[i];
 
-        System.out.printf("Enter weight (in kg): ");
-        float enterWeight = scan.nextFloat();
+                totalCost[i] = Cost;
 
-        float finalHeight = getFinalHeight(enterheight);
-
-        float bmiResult = enterWeight / finalHeight;
-
-        System.out.printf("\nResult: %.1f\n", bmiResult);
-
-        if (bmiResult < 18.0) {
-            System.out.println("UNDERWEIGHT");
+                overallTotalCost += Cost;
+            }
         }
-        else if (bmiResult < 25.0) {
-            System.out.println("NORMAL WEIGHT");
-        }
-        else if (bmiResult < 29.9) {
-            System.out.println("OVERWEIGHT");
-        }
-        else {
-            System.out.println("OBESE");
-        }
-    }
 
-    public static float getFinalHeight(float enterHeight) {
-        enterHeight = enterHeight / 100;
-        float result = enterHeight * enterHeight;
-        return result;
+        System.out.println();
+        for (int i = 0; i < numItems; i++) {
+            System.out.printf("Total Cost of item %d: %,.02f\n", i + 1, totalCost[i]);
+        }
+
+        System.out.printf("\nOverall Cost: $%,.02f\n", overallTotalCost);
     }
 }
