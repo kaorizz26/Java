80e259f4e102aae65809f8f26af2b44e7ea90833
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-10-17 22:06:15 +0800

    Deleted all line of code

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -8,25 +8,6 @@ public class commit {
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
 
-        ArrayList<String> crushes = new ArrayList<String>();
 
-        
-        int counter = 0;
-        while (true) {
-            System.out.printf("Enter your crush %d name: ", counter + 1);
-            String crushInput = scan.nextLine();
-            counter++;
-            
-            if (crushInput.equalsIgnoreCase("q")) {
-                break;
-            }
-            else {
-                crushes.add(crushInput);
-            }
-        }
-
-        int resultWife = random.nextInt(crushes.size());
-
-        System.out.printf("Your future wife will be: %s\n", crushes.get(resultWife));    
     }
 }
