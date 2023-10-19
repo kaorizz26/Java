b6ee749b858aa9c8c72102e5a4e08ccbe0f5c706
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-10-19 21:21:55 +0800

    Clear all code

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -3,20 +3,7 @@ import java.util.*;
 
 public class commit {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Random random = new Random();
 
-        String[][] foodsArray = {
-            {"Pork", "Adobo", "Chicken"},
-            {"Pizza", "Lollipop", "Barbeque"},
-            {"Apple", "Kimchi", "Mojos"}
-        };
-
-        for (int i = 0; i < foodsArray.length; i++) {
-            for (int j = 0; j < foodsArray[i].length; j++) {
-                System.out.print(foodsArray[i][j] + "    \t");
-            }
-            System.out.println();
-        }
+        
     }
 }
