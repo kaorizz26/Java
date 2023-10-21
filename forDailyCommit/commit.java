79be820d8452f9c315b3c2172546d57e2abfadee
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-10-21 21:27:34 +0800

    Practice code (beginner problems)

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -6,6 +6,18 @@ public class commit {
 
         Scanner scan = new Scanner(System.in);
         
-        
+        System.out.print("\nEnter the meal cost: ");
+        double mealCostInput = scan.nextDouble();
+
+        System.out.print("Enter tip percentage (in numbers only, ex: 10): ");
+        double tipInput = scan.nextInt();
+
+        double convertTip = tipInput / 100;
+
+        double totalBill = mealCostInput + (mealCostInput * convertTip);
+
+        System.out.printf("\nMeal Cost: $%,.02f\n", mealCostInput);
+        System.out.printf("Tip percentage: %.0f%%\n", tipInput);
+        System.out.printf("Total Bill: $%,.02f\n", totalBill);
     }
 }
