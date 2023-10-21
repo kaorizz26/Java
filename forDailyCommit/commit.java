a926e8b3b21f5bb433e786713818959f61bc386b
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-10-21 21:26:36 +0800

    clear code

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -5,15 +5,7 @@ public class commit {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
-        System.out.print("Enter distance travelled to your current destination (in km): ");
-        double distanceInput = scan.nextDouble();
-
-        System.out.print("Enter time taken to travel in current destination (in hour): ");
-        double timeTakenInput = scan.nextDouble();
-
-        double speed = distanceInput / timeTakenInput;
-
-        System.out.printf("Speed: %.02f(km/h)\n", speed);
+        
+        
     }
 }
