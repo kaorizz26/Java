ea7352e75d1be917ce6b09da4ec31bf23afafcb1
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-10-23 22:05:04 +0800

    Clear code

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -4,34 +4,6 @@ import java.util.*;
 public class commit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Random random = new Random();
-        
-        int randomNum = random.nextInt(10) + 1;
-        int counter = 5;
-        
-        for (int i = 5; i > 0; i--) {
-            
-            System.out.printf("\nGuess the number between 1 to 6 (%d tries left): ", i);
-            int guessNumInput = scan.nextInt();
-
-            if (guessNumInput < 1 || guessNumInput > 6) {
-                System.out.println("Number must be from 1 - 6!");
-                i++;
-            }
-            else {
-                if (guessNumInput == randomNum) {
-                    System.out.println("You guess it!!!");
-                    break;
-                }
-                else {
-                    System.out.println("Wrong!!!!");
-                    counter--;
-                }
-            }
-        }
-
-        if (counter == 0) {
-            System.out.println("Try again");
-        }
+      
     }
 }
