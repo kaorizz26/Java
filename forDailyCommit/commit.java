0acdca6aebb4522a1528122292f681f6ae0b3d9f
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-10-24 23:10:19 +0800

    clear code

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -4,15 +4,7 @@ import java.util.*;
 public class commit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-    
-        ArrayList<String> foods = new ArrayList<>();
-
-        foods.add("orange");
-        foods.add("apple");
-        foods.add("pineapple");
-
-        for (int i = 0; i < foods.size(); i++) {
-            System.out.println(foods.get(i));
-        }
+        
+        
     }
 }
