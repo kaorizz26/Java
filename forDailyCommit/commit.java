f80327a4d5163754451685651eebc44f2b17ef78
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-10-31 16:34:37 +0800

    Practice code (basic use of methods)

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -5,6 +5,18 @@ public class commit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        
+        System.out.print("Enter num a: ");
+        int a = scan.nextInt();
+
+        System.out.print("Enter num b: ");
+        int b = scan.nextInt();
+
+        int z = add(a, b);
+        System.out.printf("The sum is: %d\n", z);
+    }
+
+    public static int add(int a, int b) {
+        int z = a + b;
+        return z;
     }
 }
