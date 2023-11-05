65ffde2d97b4eacf8d62f887c91170cfaeea70a9
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-11-05 21:44:31 +0800

    Clear code

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -3,14 +3,7 @@ import java.util.*;
 
 public class commit {
     public static void main(String[] args) {
-        ArrayList<String> nameList = new ArrayList<>();
-
-        nameList.add("Sandra");
-
-        System.out.println(nameList.get(0));
         
-        for (String item : nameList) {
-            System.out.println(item);
-        }
+        
     }
 }
