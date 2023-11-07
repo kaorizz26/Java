110ffb865ea1a1e577d47a7efec8f4bd011390f9
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-11-07 23:31:24 +0800

    Coded some school activity (Leap year, calculator, bmi)

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -3,7 +3,29 @@ import java.util.*;
 
 public class commit {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter you height (in meters): ");
+        double height = scan.nextDouble();
+
+        System.out.print("Enter your weight (in kg): ");
+        double weight = scan.nextDouble();
+
+        double result = weight / (height * height); //* BMI result
+
+        String categoryBMI = "";
+
+        if (result < 18.5) {
+            categoryBMI = "Underweight";
+        } else if (result < 24.9) {
+            categoryBMI = "Normal Weight";
+        } else if (result < 29.9) {
+            categoryBMI = "Overweight";
+        } else {
+            categoryBMI = "Obese";
+        }
+
+        System.out.println(categoryBMI); //* output BMI category 
         
     }
 }
