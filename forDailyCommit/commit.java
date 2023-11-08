a9b38eaf0406b36550068e9c13ffb0bf80e8c1f1
kaorizz26 <iamtristanjadesanchez@gmail.com>
2023-11-08 19:29:37 +0800

    Practice code (Array)

--- forDailyCommit/commit.java
+++ forDailyCommit/commit.java
@@ -5,6 +5,10 @@ public class commit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        
+        String[] foodArray = {"adobo", "sinigang", "bbq"};
+
+        for (String food : foodArray) {
+            System.out.println(food);
+        }
     }
 }
